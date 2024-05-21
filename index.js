489e51d6bff577dea6bce5f304d615bf39bf0562
Igor Baranov <igorbaranov2017@gmail.com>
2024-05-22 00:28:36 +0200

    add logic with operators

--- index.js
+++ index.js
@@ -1,43 +1,36 @@
 const screen = document.querySelector('.calculator-screen__results');
-let expression = [];
+let valueArray = [];
+const operators = ['+', '-', '*', '/'];
 
-for(let i=0; i < 10; i++) {
-    document.querySelector('.button-' + i).addEventListener('click', function() {
-        expression += [i];
-    });
+// return true value (button on calculator) is operator
+function isOperator(value) {
+    return operators.includes(value);
 }
 
-const buttonAc = document.querySelector('.button-AC').addEventListener('click', function() {
-    expression = [];
-});
-
-const buttonDivide = document.querySelector('.button-divide').addEventListener('click', function() {
-    expression += ["/"];
-});
-const buttonMultiply = document.querySelector('.button-multiply').addEventListener('click', function() {
-    expression += ["*"];
-});
-const buttonMinus = document.querySelector('.button-minus').addEventListener('click', function() {
-    expression += ["-"];
-});
-const buttonPlus = document.querySelector('.button-plus').addEventListener('click', function() {
-    expression += ["+"];
-});
-
-const allButtons = document.querySelectorAll('button');
-for (let i = 0; i < allButtons.length; i++) {
-    allButtons[i].addEventListener('click', function() {
-        screen.innerHTML = expression.slice(0,15);
-    });
-}
-
-const buttonEqual = document.querySelector('.button-equal').addEventListener('click', function() {
-    let result = eval(expression).toString();
-    if (result.length > 15) {
-        screen.innerHTML = result.slice(0 , 15) + "...";
+function display(value) {
+// check if the last element is an operator and the new value is also an operator
+    if (isOperator(valueArray[valueArray.length - 1]) && isOperator(value)) {
+// replacing the last operator with the new operator. even when old and new are the same
+        valueArray[valueArray.length - 1] = value;
     } else {
-        screen.innerHTML = result;
+// if not, just push new number 
+        valueArray.push(value);
     }
-});
+// updating the display: joining array to string to get rig of commas  
+    screen.innerText = valueArray.join('');
+    console.log(valueArray);
+}
 
+function calculateResult() {
+    // joining again because eval works just with strings
+        let result = eval(valueArray.join(''));
+        screen.innerText = result;
+        // Save result for further calculations
+        valueArray = [result];
+}
+
+function reset() {
+    screen.innerText = '';
+    valueArray = [];
+}
 
